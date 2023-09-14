d01df4ff7e114eb1640af192678e7138855b51cd
ZooLoup <116280382+ZooLoup@users.noreply.github.com>
2023-09-14 20:45:08 +0800

    Add files via upload

---
+++ 三子棋/五子棋/game.h
@@ -0,0 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define ROW 3
+#define COL 3
+#include <stdio.h>
+void InitBoard(int board[ROW][COL], int row, int col);
+void DisplayBoard(char board[ROW][COL], int row, int col);
