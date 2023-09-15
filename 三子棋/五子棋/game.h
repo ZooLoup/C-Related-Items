c90d68b247819bac89102d6e539f8c810aeebb1d
ZooLoup <116280382+ZooLoup@users.noreply.github.com>
2023-09-15 16:25:18 +0800

    Add files via upload

--- 三子棋/五子棋/game.h
+++ 三子棋/五子棋/game.h
@@ -2,5 +2,17 @@
 #define ROW 3
 #define COL 3
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 void InitBoard(int board[ROW][COL], int row, int col);
 void DisplayBoard(char board[ROW][COL], int row, int col);
+void PlayerMove(char board[ROW][COL], int row, int col);
+void ComputerMove(char board[ROW][COL], int row, int col);
+
+//告诉我们四种游戏的状态
+//玩家赢  '*'
+//电脑赢  '#'
+//平局    'Q'
+//都没有赢'C'
+
+char IsWin(char board[ROW][COL], int row, int col);
--- 三子棋/五子棋/test.c
+++ 三子棋/五子棋/test.c
@@ -10,16 +10,46 @@ void menu()
 //游戏的整个实现
 void game()
 {
+	char ret = 0;
 	//数组-存放走出的棋盘信息
 	char board[ROW][COL] = { 0 };//全部空格
 	//初始化棋盘
 	InitBoard(board, ROW, COL);
 	//打印棋盘
 	DisplayBoard(board, ROW, COL);
+	//下棋
+	while (1)
+	{
+		//玩家下棋
+		PlayerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		//判断玩家是否赢
+		ret=IsWin(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+		//电脑下棋
+		ComputerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		//判断电脑是否赢
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+	}
+	if (ret == '*')
+		printf("玩家赢\n");
+	else if (ret == '#')
+		printf("电脑赢");
+	else
+		printf("平局");
 }
 void test()
 {
 	int input = 0;
+	srand((unsigned int)time(NULL));
 	do
 	{
 		menu();
